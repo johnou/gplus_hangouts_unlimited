6dc4e616584a7dd21a9bcf40ff8eea86a0c8690c
johnou <johno@hellface.com>
2013-11-15 11:07:26 +0100

    Loop over all windows and tabs.

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 (function() {
 
     var listen = function(url) {
-        ws = new WebSocket(url);
+        var ws = new WebSocket(url);
         ws.onmessage = function(event) {
-            chrome.tabs.getSelected(null, function(tab){
-                chrome.tabs.sendMessage(tab.id, {action: "toggle_mute"});
+            chrome.windows.getAll({ populate: true }, function(windows) {
+                windows.forEach(function(window) {
+                    window.tabs.forEach(function(tab) {
+                        chrome.tabs.sendMessage(tab.id, {action: "toggle_mute"});
+                    });
+                });
             });
         };
         ws.onclose = function(){
